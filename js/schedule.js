d7f07e992122860aa8b572db64fdd01c17cf63e8
thekamfamtx <thekamfamtx@gmail.com>
2026-07-16 12:48:19 -0500

    Create schedule.js

---
+++ js/schedule.js
@@ -0,0 +1,17 @@
+const schedule = [
+
+/*
+
+Example Event:
+
+{
+    title: "Welcome Aboard Meet-Up",
+    day: "Saturday, November 21, 2026",
+    time: "4:00 PM",
+    location: "TBD",
+    description: "Come meet fellow KAM Fam cruisers!"
+}
+
+*/
+
+];

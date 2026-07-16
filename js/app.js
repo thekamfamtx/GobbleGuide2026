e71bb4caa83d17f2c834294b13f03262adb78bb2
thekamfamtx <thekamfamtx@gmail.com>
2026-07-16 11:58:04 -0500

    Update and rename app.js to js/app.js

---
+++ js/app.js
@@ -0,0 +1,15 @@
+document
+.getElementById("themeButton")
+.addEventListener("click",function(){
+
+    // We'll put your YouTube Theme link here later.
+
+});
+
+document
+.getElementById("enterButton")
+.addEventListener("click",function(){
+
+    window.location.href="pages/home.html";
+
+});

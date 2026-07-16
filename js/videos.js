601ac46fd090a7b33384ac9a13e47a1c0f568801
thekamfamtx <thekamfamtx@gmail.com>
2026-07-16 12:34:29 -0500

    Create videos.js

---
+++ js/videos.js
@@ -0,0 +1,34 @@
+document
+.getElementById("livesButton")
+.addEventListener("click", function(){
+
+    window.open(
+        "https://youtube.com/playlist?list=PLVvH0A8WCqQ4&si=YAb1U_o_Nv2hVFpf",
+        "_blank"
+    );
+
+});
+
+
+document
+.getElementById("themeButton")
+.addEventListener("click", function(){
+
+    window.open(
+        "https://youtu.be/W-jb4GxRmuQ",
+        "_blank"
+    );
+
+});
+
+
+document
+.getElementById("channelButton")
+.addEventListener("click", function(){
+
+    window.open(
+        "https://youtube.com/@TheKAMFamTX?sub_confirmation=1",
+        "_blank"
+    );
+
+});

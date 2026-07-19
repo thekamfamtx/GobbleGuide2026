9dda736608a281a875bb5d9c95d0e6036c008469
thekamfamtx <thekamfamtx@gmail.com>
2026-07-18 23:15:29 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,18 @@
-const CACHE_NAME = "gobble-guide-test2";
+const CACHE_NAME = "gobble-guide-test3";
 
 const urlsToCache = [
   "./",
   "./index.html",
   "./manifest.json",
   "./css/style.css",
-  "./js/app.js"
+  "./js/app.js",
+
+  "./pages/home.html",
+  "./pages/cruise-info.html",
+  "./pages/schedule.html",
+  "./pages/videos.html",
+  "./pages/memories.html",
+  "./pages/merch.html"
 ];
 self.addEventListener("install", event => {
 
